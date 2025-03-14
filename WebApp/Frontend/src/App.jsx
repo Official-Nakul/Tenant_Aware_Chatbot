ac0c7bcb8b53ee27a5a91db1ae1d69a794a274f1
Official-Nakul <visavadianakulyt@gmail.com>
2025-03-14 09:43:38 +0530

    initial commit of webapp

---
+++ WebApp/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="bg-amber-400">hi</div>
+      <Button variant="outline">Button</Button>
+    </>
+  );
+}
+
+export default App;
