7c7d20fed2bba7c09082459883489abc78228a4f
Official-Nakul <visavadianakulyt@gmail.com>
2025-03-16 11:07:35 +0530

    vite.gonfig.js cors

--- WebApp/Frontend/vite.config.js
+++ WebApp/Frontend/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -9,5 +10,16 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    proxy: {
+      "/chat": {
+        target: "https://tenant-aware-chatbot.onrender.com/",
+        changeOrigin: true,
+        secure: false,
+        // rewrite: (path) => path.replace(/^\/chat/, ""),
+      },
+    },
+    cors: false, // This is not needed here; CORS is handled by the backend
   },
 });
