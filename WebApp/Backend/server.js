2b47211c0e50c0b919b624381960b421e5f6a658
Official-Nakul <visavadianakulyt@gmail.com>
2025-03-16 19:50:45 +0530

    connected to db

--- WebApp/Backend/server.js
+++ WebApp/Backend/server.js
@@ -9,13 +9,7 @@ const allowedOrigins = [
 ];
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true, // Allows any origin dynamically
     credentials: true,
   })
 );
