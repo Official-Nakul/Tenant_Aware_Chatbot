fef3c6e710023f8f856417874567a281fe03ecb9
Official-Nakul <visavadianakulyt@gmail.com>
2025-03-25 10:06:40 +0530

    production url changes cors

--- WebApp/Backend/server.js
+++ WebApp/Backend/server.js
@@ -10,17 +10,7 @@ const allowedOrigins = ["*"];
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // Allow requests with no origin (like mobile apps, curl requests)
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const msg =
-          "The CORS policy for this site does not allow access from the specified Origin.";
-        return callback(new Error(msg), false);
-      }
-      return callback(null, true);
-    },
+    origin: "*", // This truly allows all origins
     credentials: true,
   })
 );
